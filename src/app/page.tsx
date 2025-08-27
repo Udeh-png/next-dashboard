3059f28e77338c915108cef8798b3b46253ba69d
Udeh-png <leonwokedichisom@gmail.com>
2025-08-27 23:41:50 +0100

    fix up home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
 import { Card } from "@/components/Card";
+import Link from "next/link";
 
 export default function Home() {
-  return <div className="bg-red-500">Welcome to the Home Page</div>;
+  return (
+    <div className="grid grid-rows-[1fr_8fr]">
+      <Card>Welcome to the Home Page</Card>
+      <div className="flex justify-center items-center">
+        <Link
+          href="/dashboard"
+          className="px-7 py-1 rounded bg-trans-white flex flex-col items-center hover:bg-[rgba(255,255,255,0.6)] transition-colors"
+        >
+          Dashboard
+          <span className="text-[7px]">Thats what you're here for init</span>
+        </Link>
+      </div>
+    </div>
+  );
 }
